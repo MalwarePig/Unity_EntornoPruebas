4eb18be09f376bb74ef398923601d0b0132cb9bf
MalwarePig <sergio_lcc92@hotmail.com>
2023-01-13 21:31:15 -0600

    first commit

---
+++ gitPush.bat
@@ -0,0 +1,10 @@
+ e:
+
+cd E:\Proyectos Unity\3D\Unity_EntornoPruebas
+
+echo "# Unity_EntornoPruebas" >> README.md
+git init
+git add .
+git commit -m "Push automatico casa"
+git branch -M main
+git push -u origin main
